ed266d3536c0ecaffa1fc2d26c2f794482bbb7f3
Rahul purohit <rahulanuj2020@gmail.com>
2026-02-22 15:30:55 +0530

    fixed case issue

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use("/api", userRoutes);
 // Server Start
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log(`Express server running on 5000`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
