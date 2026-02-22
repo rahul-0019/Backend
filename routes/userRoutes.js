7e62d48ddb17bc157e99e3b56159bb6173d7d361
Rahul purohit <rahulanuj2020@gmail.com>
2026-02-22 17:26:01 +0530

    fixed case issue

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const user = require("../models/user");
+const User = require("../models/user");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
